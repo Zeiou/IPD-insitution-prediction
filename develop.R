a3a66e5ccb00eb73e24f7a9e073ba322bb76a295
Zeiou <78864100+Zeiou@users.noreply.github.com>
2025-05-15 11:35:24 +0100

    Update develop.R

--- develop.R
+++ develop.R
@@ -1,9 +1,9 @@
 #install.packages("rsconnect")
 
 library(rsconnect)
-rsconnect::setAccountInfo(name='yan61726',
-                          token='32ACCB2536BE4ED96E13DC03DE199F65',
-                          secret='EpqhV2tIw7xb2ZtBhGV1Z7DKUcHvjLRHapTiO7kj')
+rsconnect::setAccountInfo(name='xxxx',
+                          token='xxx5',
+                          secret='xxxxx')
 
 # Deploy app from your R project folder
 rsconnect::deployApp("C:/RiskofInstitutionalisation")
