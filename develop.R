2b424ca1f270c665060d9b11b12d73cf241e1158
Zeiou <78864100+Zeiou@users.noreply.github.com>
2025-05-30 12:17:26 +0100

    Add files via upload

--- develop.R
+++ develop.R
@@ -1,9 +1,9 @@
 #install.packages("rsconnect")
 
 library(rsconnect)
-rsconnect::setAccountInfo(name='xxxx',
-                          token='xxx5',
-                          secret='xxxxx')
+rsconnect::setAccountInfo(name='yan61726',
+                          token='32ACCB2536BE4ED96E13DC03DE199F65',
+                          secret='EpqhV2tIw7xb2ZtBhGV1Z7DKUcHvjLRHapTiO7kj')
 
 # Deploy app from your R project folder
 rsconnect::deployApp("C:/RiskofInstitutionalisation")
@@ -11,3 +11,4 @@ rsconnect::deployApp("C:/RiskofInstitutionalisation")
 rsconnect::showLogs(streaming = TRUE)
 
 #shinyApp(ui = ui, server = server) only for local
+
